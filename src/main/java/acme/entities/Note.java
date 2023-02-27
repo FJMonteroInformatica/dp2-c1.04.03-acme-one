4aff58a5822fd670dc508fb775e808c43fc5f151
Ahydul1 <ahydul1@gmail.com>
2023-02-27 18:28:16 +0100

    Create entity Note

---
+++ src/main/java/acme/entities/Note.java
@@ -0,0 +1,57 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Note extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Past
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				instantiationMoment;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 76)
+	@Pattern(regexp = "^〈\\w+〉 - 〈\\p{L}+, \\p{L}+〉$")
+	protected String			author;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			message;
+
+	@Email
+	protected String			email;
+
+	@URL
+	protected String			link;
+
+}
