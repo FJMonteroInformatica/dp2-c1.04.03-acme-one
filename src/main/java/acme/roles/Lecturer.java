60666171101210b3820bb24b2fa24efeb5ffe8d2
joscasvaz <jicastrotic99@gmail.com>
2023-03-01 21:34:47 +0100

    feat: lecturer - course relationship.
    Created a many to one relationship from lecturer to course.

--- src/main/java/acme/roles/Lecturer.java
+++ src/main/java/acme/roles/Lecturer.java
@@ -2,12 +2,16 @@
 package acme.roles;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractRole;
+import entities.course.Course;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -40,5 +44,10 @@ public class Lecturer extends AbstractRole {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Course			course;
 
 }
