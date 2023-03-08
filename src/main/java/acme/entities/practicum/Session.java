282dc8b8c3c17ea0dd9f2df99c85f05d3d46830f
Juan Lopez <juanquiroslopez@gmail.com>
2023-03-08 11:38:11 +0100

    Minor fixes

--- src/main/java/acme/entities/practicum/Session.java
+++ src/main/java/acme/entities/practicum/Session.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -40,9 +42,11 @@ public class Session extends AbstractEntity {
 	 * start at least one week ahead, at least one week long
 	 */
 	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
 	protected Date				start;
 
 	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
 	protected Date				finish;
 
 	@NotNull
