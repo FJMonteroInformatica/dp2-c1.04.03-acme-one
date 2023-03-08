44066de037a21a5fb4246602ea9c3dd92f92959b
joscasvaz <jicastrotic99@gmail.com>
2023-03-08 21:15:19 +0100

    feat: attributes.
    Added all the attributes to the class with its validations.

--- src/main/java/acme/entities/lecture/Lecture.java
+++ src/main/java/acme/entities/lecture/Lecture.java
@@ -2,6 +2,11 @@
 package acme.entities.lecture;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
@@ -14,9 +19,29 @@ public class Lecture extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 
-	protected static final long serialVersionUID = 1L;
+	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Max(value = 76)
+	protected String			title;
+
+	@NotBlank
+	@Max(value = 101)
+	protected String			_abstract;
+
+	@Min(value = 1)
+	protected int				learningHours;
+
+	@NotBlank
+	@Max(value = 101)
+	protected String			body;
+
+	protected boolean			isHandsOn;
+
+	@URL
+	protected String			link;
 
 	// Derived attributes -----------------------------------------------------
 
